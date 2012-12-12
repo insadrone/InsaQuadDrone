e8bd063be5e897dbf599569558fd4ca6ac8e9236
huynhnduc <huynhnduc100@yahoo.com.vn>
2012-12-12 09:40:19 +0100

    Update sdk_apps/auto_flight/src/Comm/gps_comm.h

--- sdk_apps/auto_flight/src/Comm/gps_comm.h
+++ sdk_apps/auto_flight/src/Comm/gps_comm.h
@@ -11,14 +11,24 @@ typedef struct comm_datas_t {
   char gpgga_string[100];
 } comm_datas;
 
+typedef struct comm_datas_t_target {
+  char gprmc_string[100];
+  char gpgga_string[100];
+} comm_datas_target;
+
 //extern int srf_received;
 
 void init_comm();
 void stop_comm();
 comm_datas get_comm_datas();
+comm_datas_target get_comm_datas_target();
+void record_data(char *buf);
+void record_data_target(char *buf);
 char* get_coords_target();
 char* get_coords_uav();
 int start_comm(void);
+
+
 
 PROTO_THREAD_ROUTINE(receive_gps, data);
 
