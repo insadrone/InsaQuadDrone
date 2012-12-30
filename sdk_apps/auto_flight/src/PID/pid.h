cc1df088d03afedc00576f7c03d0b8c21ee37386
Flo <flo@Arcab.(none)>
2012-12-30 16:46:31 +0100

    modified:   sdk_apps/auto_flight/src/PID/pid.c 	modified:   sdk_apps/auto_flight/src/PID/pid.h

--- sdk_apps/auto_flight/src/PID/pid.h
+++ sdk_apps/auto_flight/src/PID/pid.h
@@ -4,8 +4,18 @@
 
 #define T_ECH 1
 
+/*
+                      ===== PID =====
 
+    commande(n) = P * [ erreur + (T_ECH * somme_erreur / I) + (D * (erreur(n)-erreur(n-1)) / T_ECH ) ]
+<=>
+    commande(n) = commande(n-1) + P * (erreur(n)-erreur(n-1)) + I * T_ECH * erreur(n)
+                  + D / T_ECH * (erreur(n) - 2*erreur(n-1) + erreur(n-2))
+
+  with erreur = consigne - val_capt
+
+    init_pid function reset calculations (i.e. the sum and derivative parts)
+*/
 void init_pid(void);
-
 
 double calcul_commande_pid(double consigne, double val_capt);
